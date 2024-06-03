70ce20c37d3e73308ff6742f56fa91f45e1b953f
tschoemaker <tschoemaker@uni-osnabrueck.de>
2024-06-04 00:16:36 +0200

    with errors

---
+++ SignedInputParameters.java
@@ -0,0 +1,23 @@
+public class SignedInputParameters extends TransactionInputParameters {
+    private final TransactionOutPoint prevOut;
+    private final Coin amount;
+    private final ECKey sigKey;
+    private final SigHash sigHash;
+    private final boolean anyoneCanPay;
+
+    public SignedInputParameters(TransactionOutPoint prevOut, Script scriptPubKey, Coin amount, ECKey sigKey,
+                                 SigHash sigHash, boolean anyoneCanPay) {
+        super(prevOut.getHash(), prevOut.getIndex(), scriptPubKey);
+        this.prevOut = prevOut;
+        this.amount = amount;
+        this.sigKey = sigKey;
+        this.sigHash = sigHash;
+        this.anyoneCanPay = anyoneCanPay;
+    }
+
+    public TransactionOutPoint getPrevOut() { return prevOut; }
+    public Coin getAmount() { return amount; }
+    public ECKey getSigKey() { return sigKey; }
+    public SigHash getSigHash() { return sigHash; }
+    public boolean isAnyoneCanPay() { return anyoneCanPay; }
+}
